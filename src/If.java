d486e96389ef1d5f5de98adb41367df364a1f7ee
Alexander Gorsc <alexandergorsc@gmail.com>
2023-02-26 20:50:12 +0300

    add If

---
+++ src/If.java
@@ -0,0 +1,14 @@
+public class If {
+    public static void main(String[] args){
+        int myInt = 15;
+        if(myInt <10){
+            System.out.println("yes, it's true");
+        }
+        else if (myInt>20){
+            System.out.println("no it's false");
+        }
+        else{
+            System.out.println("None of the previous cases");
+        }
+    }
+}
