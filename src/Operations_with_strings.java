90282d21e90178e6a387755a1e423b41cb0e4719
Alexander Gorsc <alexandergorsc@gmail.com>
2023-02-26 20:01:25 +0300

    add Operations_with_strings

---
+++ src/Operations_with_strings.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+
+public class Operations_with_strings {
+    public static void main(String[] args){
+        String m = "Hello";
+        String spase = " ";
+        String name = "Bob";
+        System.out.println(m + spase + name);
+        System.out.println("Hello" + " " + "John");
+        System.out.println("My number is num");
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String [] args){
+        int num;
+        num = 61;
+        System.out.println(num);
+
+    }
+}
