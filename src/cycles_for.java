ac3047e40085771117c81109571b46c9cdbf4290
Alexander Gorsc <alexandergorsc@gmail.com>
2023-02-26 20:39:55 +0300

    add cycles_for

---
+++ src/cycles_for.java
@@ -0,0 +1,7 @@
+public class cycles_for {
+    public static void main(String[] args){          // переменная - счетчик
+        for(int i = 10; i>=0 ; i-=5){                // i++ == i = i+1 (true)
+            System.out.println("Hello "+ i);
+        }
+    }
+}
